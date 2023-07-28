1892ac18e58394863597b262758f3a2c086b9cb0
Big-oh-learn <shubhamingale779@gmail.com>
2023-07-28 15:48:35 +0530

    Creates useProfile Hook

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,13 +1,3 @@
-import { createContext, useContext, useEffect, useState } from "react";
-
-const ProfileContext = createContext();
-export const ProfileProvider = ({ children }) => {
-    const [profile, setProfile] = useState(null);
-    const [loading, setLoading] = useState(true);
-    useEffect(()=>{
-
-    },[])
-}
 export const useProfile = () => {
     return false;
 }
