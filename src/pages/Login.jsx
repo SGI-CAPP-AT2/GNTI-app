c113a410a0a2ca4a33b22712e7a99d8a6cc14f74
Big-oh-learn <shubhamingale779@gmail.com>
2023-07-28 19:50:01 +0530

    Sign In Page Markup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,30 @@
-import { Box } from '@primer/react';
-
+import { Box, Button, Text } from '@primer/react';
+import styled from 'styled-components';
+const LoginScreen = styled(Box)`
+  width: 100%;
+  max-width: 500px;
+  padding: 10px;
+  margin: auto;
+  margin-top: 20px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+`;
 const Login = () => {
-  return <Box>Hey</Box>;
+  return (
+    <Box display="flex" alignItems="center">
+      <LoginScreen
+        border="1px solid"
+        borderColor="border.default"
+        borderBottomColor="border.default"
+      >
+        <Text textAlign="center">Sign in to continue</Text>
+        <img src="/logo512.png" />
+        <Button variant="outline" block>
+          Continue With Google
+        </Button>
+      </LoginScreen>
+    </Box>
+  );
 };
 export default Login;
