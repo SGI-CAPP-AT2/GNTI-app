f3248af350ccb9642cd6b12c5de0182d82242664
Big-oh-learn <shubhamingale779@gmail.com>
2023-07-29 11:02:06 +0530

    firebaseapp.js created & stored API key

---
+++ src/firebase/firebaseapp.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyD5Wg0rqw3nTV2PK1CBc0rF5m1wl45tyTU',
+  authDomain: 'gnti-at-adypsoe.firebaseapp.com',
+  databaseURL:
+    'https://gnti-at-adypsoe-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'gnti-at-adypsoe',
+  storageBucket: 'gnti-at-adypsoe.appspot.com',
+  messagingSenderId: '108176675437',
+  appId: '1:108176675437:web:fcbaec3fae96b8f6cd6564',
+};
+const app = initializeApp(firebaseConfig);
+export const auth = app.auth();
+export const database = app.database();
+export const storage = app.storage();
