b68264c23e6ef6f123b6ff0e1f5ec32549ee89f3
Big-oh-learn <shubhamingale779@gmail.com>
2024-01-31 09:40:18 +0530

    Added Link a Device Page

--- src/pages/Verifier/DeviceLink.jsx
+++ src/pages/Verifier/DeviceLink.jsx
@@ -1,4 +1,118 @@
+import { Box } from '@primer/react';
+import { useEffect, useState } from 'react';
+import QrReader from 'react-qr-scanner';
+import OptionPageBar from '../../components/OptionPageBar';
+import { useNavigate } from 'react-router-dom';
+import { useScreen } from '../../context/screen.context';
+import AfterScan from './components/AfterScan';
+import { useProfile } from '../../context/profile.context';
+import { database } from '../../firebase/firebaseapp';
+import Loader from '../../components/Loader';
 const DeviceLink = () => {
-  return <div>Link a Device</div>;
+  const [isScanning, setIsScanning] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [scanRes, setScanResults] = useState(null);
+  const [alreadyLinked, setAlreadyLinked] = useState(false);
+  const { profile } = useProfile();
+  const scanning = res => {
+    if (res) {
+      let found_res = new URL(res.text);
+      let bmfq_id = found_res.searchParams.get('bmfqid');
+      console.log(found_res);
+      if (bmfq_id && found_res.host === 'gnti-at-adypsoe.web.app') {
+        setScanResults(found_res.searchParams.get('bmfqid'));
+        setIsScanning(false);
+      }
+    }
+  };
+  const { height, width } = useScreen();
+  const [barH, setBarH] = useState(0);
+  const appWidth = width > 750 ? 750 : width - 20;
+  const navigate = useNavigate();
+  useEffect(() => {
+    const profileRef = database.ref('/profiles/' + profile.uid);
+    const mn = async () => {
+      const linkedSessionSnap = await profileRef
+        .child('session_linked')
+        .once('value');
+      const sessionRef = database.ref('/sessions/' + linkedSessionSnap.val());
+      const stateOfSession = await sessionRef.child('state').once('value');
+      console.log(stateOfSession.val());
+      if (stateOfSession.val() == 'active') {
+        setAlreadyLinked(true);
+      }
+      setIsLoading(false);
+    };
+    mn();
+  });
+  return (
+    <>
+      <OptionPageBar
+        text={'Link a Device to start a Session'}
+        buttons={true}
+        onCancel={() => {
+          navigate('/');
+        }}
+        setAquiredHeight={setBarH}
+      />
+      {isLoading && (
+        <Box width={appWidth} margin={'auto'} marginTop={10}>
+          <Loader width={appWidth} />
+        </Box>
+      )}
+      {!isLoading && !alreadyLinked && isScanning && (
+        <Box
+          width={appWidth}
+          height={height - barH}
+          display={'flex'}
+          overflowY={'scroll'}
+          margin={'auto'}
+        >
+          <Box
+            margin={'auto'}
+            width={(appWidth < height - barH ? appWidth : height - barH) - 4}
+            height={(appWidth < height - barH ? appWidth : height - barH) - 4}
+            overflow={'hidden'}
+            display={'flex'}
+            justifyContent={'center'}
+            border={'2px solid'}
+            borderColor={'border.subtle'}
+            borderRadius={'30px'}
+          >
+            <QrReader
+              style={{
+                height: appWidth,
+                display: 'flex',
+              }}
+              delay={100}
+              onScan={scanning}
+              onError={() => {}}
+            />
+          </Box>
+        </Box>
+      )}
+      {!isLoading && !isScanning && (
+        <Box width={appWidth} height={height - barH} margin={'auto'}>
+          <AfterScan bmfq={scanRes} height={height - barH} width={appWidth} />
+        </Box>
+      )}
+      {!isLoading && alreadyLinked && (
+        <Box
+          width={appWidth}
+          height={height - barH}
+          margin={'auto'}
+          display={'flex'}
+          flexDirection={'column'}
+        >
+          <Box margin={'auto'} marginY={'10px'}>
+            Already a Session started
+          </Box>
+          <Box margin={'auto'} marginY={'10px'} fontSize={'small'}>
+            Manage it from the {'"Set Session Status"'}
+          </Box>
+        </Box>
+      )}
+    </>
+  );
 };
 export default DeviceLink;
---
+++ src/pages/Verifier/components/AfterScan.jsx
@@ -0,0 +1,96 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
+import Loader from '../../../components/Loader';
+import { Box, Button, FormControl, Radio, RadioGroup } from '@primer/react';
+import { useNavigate } from 'react-router-dom';
+import { database } from '../../../firebase/firebaseapp';
+import { useProfile } from '../../../context/profile.context';
+const AfterScan = ({ height, width, bmfq }) => {
+  const [isLoding, setIsLoading] = useState(true);
+  const [allRoutes, setAllRoutes] = useState(null);
+  const navigate = useNavigate();
+  const [selectedRoute, setSelectedRoute] = useState(null);
+  const bottomSheet = useRef(null);
+  const [btmShtHgt, setBtmHeigh] = useState(0);
+  const [isCreatingSession, setIsCreatingSession] = useState(false);
+  const { profile } = useProfile();
+  useEffect(() => {
+    if (!isCreatingSession) {
+      const mn = async () => {
+        const response = await fetch(
+          'http://127.0.0.1:5001/gnti-at-adypsoe/us-central1/busApi?q=RETURN_ALL_ROUTES_1109'
+        );
+        const allRoutes = await response.json();
+        setAllRoutes(allRoutes);
+        setIsLoading(false);
+      };
+      mn();
+    }
+  }, [isCreatingSession]);
+  useLayoutEffect(() => {
+    if (bottomSheet.current) {
+      setBtmHeigh(bottomSheet.current.offsetHeight);
+    }
+  }, [bottomSheet, height, isLoding]);
+  const setRouteFromGroup = v => {
+    setSelectedRoute(v);
+  };
+  const createSession = async () => {
+    if (selectedRoute) {
+      setIsCreatingSession(true);
+      setIsLoading(true);
+      const sessionsRef = database.ref('/sessions/');
+      const deviceRef = database.ref('/BMfQs/' + bmfq);
+      const profileRef = database.ref('/profiles/' + profile.uid);
+      const sessionSnap = await sessionsRef.once('value');
+      const id = sessionSnap.numChildren() + 1;
+      await sessionsRef.child(id).set({
+        id,
+        index: 0,
+        route: selectedRoute,
+        state: 'active',
+      });
+      await deviceRef.child('session').set(id);
+      await profileRef.child('session_linked').set(id);
+      navigate('/');
+      console.log(bmfq);
+    }
+  };
+  return (
+    <Box width={width}>
+      {isLoding && (
+        <Box margin={'auto'}>
+          <Loader />
+        </Box>
+      )}
+      {!isLoding && (
+        <>
+          <Box height={height - btmShtHgt - 10}>
+            <RadioGroup name="choiceGroup" onChange={setRouteFromGroup}>
+              <RadioGroup.Label>
+                Choose the route to start session on:{' '}
+              </RadioGroup.Label>
+              {allRoutes.map(({ name, id }) => (
+                <FormControl key={id}>
+                  <Radio value={id} />
+                  <FormControl.Label>{id + ' ' + name}</FormControl.Label>
+                </FormControl>
+              ))}
+            </RadioGroup>
+          </Box>
+          <Box ref={el => (bottomSheet.current = el)} display={'flex'}>
+            <Box>
+              <Button variant="danger" onClick={() => navigate('/')}>
+                Cancel
+              </Button>
+            </Box>
+            <Box marginLeft={'auto'}>
+              <Button onClick={createSession}>Continue</Button>
+            </Box>
+          </Box>
+        </>
+      )}
+    </Box>
+  );
+};
+export default AfterScan;
